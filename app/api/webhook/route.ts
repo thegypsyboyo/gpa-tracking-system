906584d65c323d77b4198ee315296ab735cb3b86
thegypsyboyo <lewisonyango9@gmail.com>
2024-01-14 17:32:05 +0300

    added webhook to env.local

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
 
     //   TODO: Add your webhooks to secret .env file
-    const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRE
+    const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET
 
     if (!WEBHOOK_SECRET) {
         throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
